58d7743e7a3b5c91b1acddf5a2cbd68e0d2be022
danieltijerina <dtg.danieltijerina@gmail.com>
2018-11-16 14:45:04 -0600

    Bug-fix related to the use of deprecated components (ServiceDetailsTab)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,15 +6,15 @@ import { CoreModule } from '@app/core/core.module';
 import { SharedModule } from '@app/shared/shared.module';
 import { ClientTabComponent } from './components/client-tab/client-tab.component';
 import { TaxiTabComponent } from './components/taxi-tab/taxi-tab.component';
-import { ServiceDetailsTabComponent } from './components/service-details-tab/service-details-tab.component';
+//import { ServiceDetailsTabComponent } from './components/service-details-tab/service-details-tab.component';
 import { ClientTabContainerComponent } from './containers/client-tab-container/client-tab-container.component';
 import { TaxiTabContainerComponent } from './containers/taxi-tab-container/taxi-tab-container.component';
-import { ServiceDetailsTabContainerComponent } from './containers/service-details-tab-container/service-details-tab-container.component';
+//import { ServiceDetailsTabContainerComponent } from './containers/service-details-tab-container/service-details-tab-container.component';
 import { DetailsTabContainerComponent } from './containers/details-tab-container/details-tab-container.component';
 import { DetailsTabComponent } from './components/details-tab/details-tab.component';
 
 @NgModule({
-  declarations: [AdminComponentComponent, AdminContainerComponent, ClientTabComponent, TaxiTabComponent, ServiceDetailsTabComponent, ClientTabContainerComponent, TaxiTabContainerComponent, ServiceDetailsTabContainerComponent, DetailsTabContainerComponent, DetailsTabComponent],
+  declarations: [AdminComponentComponent, AdminContainerComponent, ClientTabComponent, TaxiTabComponent, ClientTabContainerComponent, TaxiTabContainerComponent, DetailsTabContainerComponent, DetailsTabComponent],
   imports: [
     CommonModule, CoreModule, SharedModule
   ]
